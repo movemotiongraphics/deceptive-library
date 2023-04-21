43003ccbc7263ba6a91f76dbe342eb890d81f5c5
movemotiongraphics <67390179+movemotiongraphics@users.noreply.github.com>
2023-04-22 02:09:28 +0800

    fe

--- pages/outcomes.jsx
+++ pages/outcomes.jsx
@@ -148,7 +148,7 @@ const ChartsPage = () => {
       <Container size="xl" px={30}>
           <Group mt={130} mb={170} position="center" alignItems="center">
             <h1 className={classes.headerTitle} style={{ textAlign: "center"}}>
-              16 interesting insights when making deceptive interfaces.
+              12 interesting insights when making deceptive interfaces.
             </h1>
           </Group>
 
@@ -279,6 +279,111 @@ const ChartsPage = () => {
                 </Grid.Col>
               </Grid>
 
+              <Grid gutter={20} mb={80}>
+                <Grid.Col span={12} md={4} sm={6}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={6} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 7</Text>
+                      <Stack>
+                          <Text fz="md">Explicitly explaining why someone donates leaves a positive impression that influences others.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+
+                <Grid.Col span={12} md={4} sm={6} xs={12}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={8} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 8</Text>
+                      <Stack mt="auto">
+                          <Text fz="md">Knowing that someone donated before you will make you feel more responsible to donate.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+
+                <Grid.Col span={12} md={4} sm={6} xs={12}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={9} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 9</Text>
+                      <Stack mt="auto">
+                          <Text fz="md">Choices can create is a sense of inferiority and a tendency to avoid easy options.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+              </Grid>
+
+              <Grid gutter={20} mb={80}>
+                <Grid.Col span={12} md={4} sm={6}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={9} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 10</Text>
+                      <Stack>
+                          <Text fz="md">Adding a gift to a donation adds a sense of significance and effort.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+
+                <Grid.Col span={12} md={4} sm={6} xs={12}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={10} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 12</Text>
+                      <Stack mt="auto">
+                          <Text fz="md">Defaults are known as "acceptable" donations.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+
+                <Grid.Col span={12} md={4} sm={6}>
+                  <Stack>
+                    <Stack className={classes.greyBackground}>
+                      <div className={classes.imageBorder}>
+                        <RiveAnimation scenarioNumber={4} hoverToPlay/>
+                      </div>
+                    </Stack>
+
+                    <Stack>
+                      <Text fz="xs" className={classes.subTitle}>Insight 13</Text>
+                      <Stack>
+                          <Text fz="md">People want to predict what will happen to their money.</Text>
+                      </Stack>
+                    </Stack>
+                  </Stack>
+                </Grid.Col>
+              </Grid>
 
             </Group>
 
